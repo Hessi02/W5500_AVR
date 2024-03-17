26d9ed6cfbdbcbd4ea93dbca76c4692366461c5e
Hessi02 <luca.hesselbrock@web.de>
2024-03-17 10:54:43 +0100

    Header guard and basic description for include file

---
+++ include/w5500.hpp
@@ -0,0 +1,17 @@
+/**
+ *  \file   w500.hpp
+ *  \brief  This is the include file for the W5500_ARV library.
+ * 
+ *  Include this file to use the library in your project. Make sure
+ *  to link the library in CMake. The library is supposed to be 
+ *  compiled with the avr-gcc compiler.
+ */
+
+#ifndef __W5500_HP__
+#define __W5500_HP__
+
+namespace W5500
+{
+};
+
+#endif //__W5500_HP__

7dd2ea1708139ec8b57533a7f25e9dd360d52a37
Hessi02 <luca.hesselbrock@web.de>
2024-03-17 12:37:08 +0100

    Added private read/write register method declarations

--- src/chip/wiznet_w5500.hpp
+++ src/chip/wiznet_w5500.hpp
@@ -6,11 +6,41 @@
 #ifndef __WIZNET_W5500_HPP__
 #define __WIZNET_W5500_HPP__
 
+#include <stdint.h>
+
 /**
  *  \class  W5500
  *  \brief  The class represents the Wiznet W5500 chip. 
  */
 class W5500
-{};
+{
+private:
+		/**
+		 *  \fn         writeRegister()
+		 *  \brief      Writes the passed data to the specified register address.
+		 *  \param[in]  addressWord passes the register address to write to.
+		 *  \param[in]  controlByte passes options for selecting various parameters.
+		 *  \param[in]  dataByteArray passes the data to send as an byte array.
+		 *  \param[in]  dataByteCount passes the length of the byte array.
+		 */
+		void writeRegister(
+			const uint16_t& addressWord,
+			const uint8_t& controlByte,
+			const uint8_t* dataByteArray,
+			const uint8_t& dataByteCount) const;
+
+		/**
+		 * 	\fn			readRegister()
+		 * 	\brief 		Reads the data of the specified register address.
+		 *  \param[in]  addressWord passes the address to read from.
+		 *  \param[in]  controlByte passes options for selecting various parameters.
+		 *  \param[in]  byteCount passes the length of the byte array to read.
+		 *	\return		Pointer to a C array containing the registers data.
+		 */
+		uint8_t* readRegister(
+			const uint16_t& addressWord,
+			const uint8_t& controlByte,
+			const uint8_t& byteCount) const;
+};
 
 #endif //__WIZNET_W5500_HPP__
